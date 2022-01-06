1f2391a2377c91812e8c526123e05608d0dd8a34
YuliyaShu <juliette7373@gmail.com>
2022-01-06 15:09:35 +0300

    docs: modify index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    n = Math.abs(n);
+    let nLength = String(n).length; 
+    let result = '';
+    for (let i = 0; i <= (nLength - 1); i++) { 
+      result = `${ String(n)[i]}` + result; 
+    };
+    return Number(result);
+  }
